f81af55be7bd61dd646ea9f378f2d9aa04a03988
Tomas Barry <tomas.barry@hotmail.com>
2017-01-20 16:28:36 +0000

    Fixed bug in auth init

--- file-server/init.js
+++ file-server/init.js
@@ -2,21 +2,23 @@ const fs = require('fs');
 const auth_socket = require('net').Socket();
 const dir_socket = require('net').Socket();
 
-const socketVariables = require('./socketVariables.js');
+const socket_variables = require('./socket_variables.js');
 
 const file_server_name = process.argv[3];
-const publicKey = fs.readFileSync(__dirname + '/pubkey.pem', 'utf8');
-
+const public_key = fs.readFileSync(__dirname + '/pubkey.pem', 'utf8');
 
 const init = {
 	init: function() {
-		auth_socket.connect(socketVariables.auth_serverPort, socketVariables.auth_serverIPAddress);
-		auth_socket.write('ADD\nfileserver' + file_server_name + '\n' + publicKey);
-		auth_socket.destroy(); 
+		console.log('Initializing file server');
+		console.log('Public Key: ' + public_key);
+		auth_socket.connect(socket_variables.auth_server_port, socket_variables.auth_server_IP_address);
+		auth_socket.write('ADD\nfileserver' + file_server_name + '\n' + public_key);
+//		auth_socket.destroy();
 
 		//dir_socket.connect(socketVariables.dir_serverPort, socketVariables.dir_serverIPAddress); 
 		//dir_socket.write('TODO');
 		//dir_socket.destroy();
+		console.log('File server initialized');
 	}
 };
 
