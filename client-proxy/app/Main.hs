940a457c1ad4c7fbaebfae95ea526483c998bbe0
Tomas Barry <tomas.barry@hotmail.com>
2017-01-22 16:43:18 +0000

    Updated client to work in system

--- client-proxy/app/Main.hs
+++ client-proxy/app/Main.hs
@@ -4,22 +4,24 @@ import System.Environment (getArgs)
 import qualified Network.Socket as Socket
 import qualified Network.Socket.ByteString as BS
 import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString as B
 
-data Address = Address String
-data Port = Port String
+
+type Address = String
+type Port = String
 data Server = Server (Address, Port)
 
 
-directoryServer :: String -> String -> Server
-directoryServer ip port = Server (Address ip, Port port)
+directoryServer :: Address -> Port -> Server
+directoryServer ip port = Server (ip, port)
 
 
 printUsageInstructions :: IO ()
 printUsageInstructions = do
     Prelude.putStrLn "What is your command?"
     Prelude.putStrLn "You can either get or put a file"
-    Prelude.putStrLn "GET <FILE NAME>"
-    Prelude.putStrLn "PUT <FILE_NAME>"
+    Prelude.putStrLn "GET\n<FILE NAME>"
+    Prelude.putStrLn "PUT\n<FILE_NAME>\n<FILE_CONTENTS>"
 
 
 setupClientSocket :: String -> String -> IO Socket.Socket
@@ -30,9 +32,15 @@ setupClientSocket ip port = do
 
 connectToRemoteSocket :: Socket.Socket -> String -> String -> IO ()
 connectToRemoteSocket socket ip port = do
-    let Server (Address a, Port p) = directoryServer ip port
+    let Server (a, p) = directoryServer ip port
     addr:_ <- Socket.getAddrInfo Nothing (Just a) (Just p)
     Socket.connect socket (Socket.addrAddress addr)
+
+readSock :: Socket.Socket -> Int -> IO [B.ByteString] -> IO B.ByteString 
+readSock _ 0 bytes = fmap B.concat bytes
+readSock socket _ message = do
+    msg <- BS.recv socket 1
+    readSock socket (B.length msg) $ message >>= (\p -> return(p ++ [msg]))
 
 
 main :: IO ()
@@ -41,7 +49,11 @@ main = do
     socket <- setupClientSocket lip lport
     connectToRemoteSocket socket rip rport
     printUsageInstructions
-    command <- getLine
+    let command = "GET\na.txt"
+    putStrLn command
     BS.sendAll socket $ C.pack command
+    response <-  readSock socket 1 (return [])
+    re <- return $ C.unpack response 
+    writeFile "/a.txt" re
     Socket.close socket
 
