373532c5fdc1cd7cc5ca466b91654850773e9ef9
Tomas Barry <tomas.barry@hotmail.com>
2017-01-22 18:35:00 +0000

    Better handling of requests

--- file-server/file-server.js
+++ file-server/file-server.js
@@ -1,8 +1,23 @@
 const fs = require('fs');
+const auth_socket = require('net').Socket();
+const socket_variables = require('./socket_variables.js');
+auth_socket.connect(socket_variables.auth_server_port, socket_variables.auth_server_IP_address);
+let pubkeys = {};
+
+auth_socket.on('data', (data) => {
+	let lines = data.split('\n');
+	let bytes = base64.decode(lines[2]);
+	pubkeys[1] = utf8.decode(bytes);
+});
 
 const file_server = {
 
 	listFiles: function(socket, message) {
+		let request = {
+			command: message[0],
+			username: message[1]
+		};
+		auth_socket.write('GET\n' + username);
 		fs.readdir(__dirname + '/Files/', (err, files) => {
 			if (err) {
 				socket.write('Error reading files in /Files/');
